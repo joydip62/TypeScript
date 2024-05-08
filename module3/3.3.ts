61c8c78549d8952054f78f1be9e1cf365685f88d
unknown <joydipbanik6@gmail.com>
2024-05-08 18:09:40 +0530

    type guard using typeof & in

--- module3/3.3.ts
+++ module3/3.3.ts
@@ -1,6 +1,55 @@
 {
-    //
+    // type guards
 
+    // type of -> type guard
+
+    type Alphanumeric = string | number;
+
+    const add = (
+        param1: Alphanumeric,
+        param2: Alphanumeric
+    ): Alphanumeric => {
+        if (typeof param1 === 'number' && typeof param2 === 'number') {
+            return param1 + param2
+        } else {
+            return param1.toString() + param2.toString();
+        }
+    };
+    
+    const res = add(123, 123);
+    // console.log(res);
+
+
+    // in guard
+
+    type NormalUser = {
+        name: string;
+    }
+
+    type AdminUser = {
+        name: string;
+        role: 'admin';
+    }
+
+    const getUser = (user: NormalUser | AdminUser) => {
+        if ('role' in user) {
+            console.log(`this is ${user.name} and role is: ${user.role}`);
+        } else {
+            console.log(`this is normal user ${user.name}`);
+        }
+    }
+    
+    const normalUser: NormalUser = {
+        name: 'Normal user'
+    }
+    
+    const adminUser: AdminUser = {
+        name: 'Admin user',
+        role: 'admin'
+    }
+
+    getUser(normalUser);
+    // getUser(adminUser);
     
     
 
