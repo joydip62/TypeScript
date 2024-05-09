e3909834ce1a680cd5c57032fa30ac585b4e01c5
JPS Archives IT <joydipbanik6@gmail.com>
2024-05-09 18:32:12 +0530

    understading static

---
+++ module3/3.7.ts
@@ -0,0 +1,19 @@
+{
+    // static
+    class Counter{
+        static count: number = 0;
+        static increment() {
+            return (Counter.count += 1)
+        }
+        static decrement() {
+            return (Counter.count -= 1)
+        }
+    }
+
+    // const instance1 = new Counter();
+    console.log(Counter.increment());
+    console.log(Counter.increment());
+
+
+    // 
+}
