16dc5ade0d018b214ea32f2cf3d0b9f72e97d982
JPS Archives IT <joydipbanik6@gmail.com>
2024-05-15 16:49:17 +0530

    node folder change

--- Mission2/module7/7.2/index.js
+++
@@ -1,9 +0,0 @@
-// local modules
-const { a, add } = require('./local-1');
-const { a: a2, add: add2 } = require('./local-2');
-
-console.log(add2(1,2,3));
-console.log(add(1, 2))
-
-
-// built-in modules
